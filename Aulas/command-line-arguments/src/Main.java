18f143d0e20071ef1c2b405ef174a35438c1bfe1
Rolucatelli <rolucatelli@gmail.com>
2024-03-07 11:52:23 -0300

    Aula 02/03

--- Aulas/command-line-arguments/src/Main.java
+++ Aulas/command-line-arguments/src/Main.java
@@ -2,9 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         if (args.length > 0){
-        for (int i = 0; i < args.length; i++){
-            System.out.println(args[i]);
-        }
+
+            for (int i = 0; i < args.length; i++){
+                System.out.println(args[i]);
+            }
+
         }
     }
 }
